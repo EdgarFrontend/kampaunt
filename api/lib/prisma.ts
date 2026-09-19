528aa01ae3ee1584904422252ecdc70f0531254b
Edgar Varderesyan <batrufdh@gmail.com>
2026-09-19 15:03:31 +0300

    fix: resolve TS errors in Vercel API build

--- api/lib/prisma.ts
+++ api/lib/prisma.ts
@@ -1,13 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
+  // @ts-ignore
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
+  // @ts-ignore
   global.prisma ||
   new PrismaClient({
     log: ['query'],
   });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+// @ts-ignore
+if (process.env.NODE_ENV !== 'production') {
+  // @ts-ignore
+  global.prisma = prisma;
+}
